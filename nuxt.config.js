a1171de6091530dc32302f03206e08e01b9fd28b
t.omi <t.omi@mosa-architect.com>
2020-04-27 01:16:00 +0900

    commit05

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,11 +16,11 @@ export default {
       { type: 'text/javascript', src: 'https://code.jquery.com/jquery-3.1.0.min.js' },
       { type: 'text/javascript', src: '//webfont.fontplus.jp/accessor/script/fontplus.js?3YlybV6urQ4%3D&box=tlruxgb88pk%3D&aa=1&ab=2', charset: 'utf-8' },
       { src: '/slick/slick.min.js' },
-      { src: '/index-loader.js' },
+      { src: '/slideshow.js' },
+      { src: '/fontReload.js' },
       { src: '/loader.js' },
       { src: '/hamburger.js' },
-      { src: '/page-top.js' },
-      { src: '/slideshow.js' }
+      { src: '/page-top.js' }
     ]
   },
   /*
