96722a5cebe9fd7d692acbad2de58c3f25cd13f0
RadishJP <kouta.hara713@gmail.com>
2021-05-17 19:57:39 +0900

    諸々更新対応及び軽微な修正。

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     ],
     script: [
       { type: 'text/javascript', src: 'https://code.jquery.com/jquery-3.1.0.min.js' },
-      { type: 'text/javascript', src: '//webfont.fontplus.jp/accessor/script/fontplus.js?3YlybV6urQ4%3D&box=tlruxgb88pk%3D&aa=1&ab=2', charset: 'utf-8' },
+      { type: 'text/javascript', src: 'https://webfont.fontplus.jp/accessor/script/fontplus.js?3YlybV6urQ4%3D&box=tlruxgb88pk%3D&aa=1&ab=2', charset: 'utf-8' },
       { src: '/slick/slick.min.js' },
       { src: '/index-loader.js' }
     ]
@@ -43,7 +43,7 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
-    
+
   ],
   /*
   ** Build configuration
--- static/index-loader.js
+++ static/index-loader.js
@@ -3,7 +3,7 @@ $('head').append(
     );
       
     jQuery.event.add(window,"load",function() { // 全ての読み込み完了後に呼ばれる関数
-        var pageH = $("#wrap").height();
+        let pageH = $("#wrap").height();
       
         $("#fade").css("height", pageH).delay(900).fadeOut(800);
         $("#loader").delay(600).fadeOut(300);
